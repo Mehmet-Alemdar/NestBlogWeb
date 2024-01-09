b9a2f46ab350b70d0001be193ea71eb534ab205a
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2024-01-09 18:13:41 +0300

    Create current date component

---
+++ src/components/CurrentDate.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="text-sm">
+    <p class="font-bold	">{{ formattedDay }}</p>
+    <p class="font-extralight">{{ formattedDate }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const formattedDate = ref('');
+const formattedDay = ref('');
+onMounted(() => {
+  const currentDate = new Date();
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  const dayOptions = { weekday: 'long' };
+  formattedDay.value = currentDate.toLocaleDateString('en-US', dayOptions);
+  formattedDate.value = currentDate.toLocaleDateString('en-US', options);
+});
+</script>
