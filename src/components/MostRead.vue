a009854a04afd5bd3efaf063e84c0d3d35fb1196
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2024-01-10 13:56:44 +0300

    Update MostRead.vue

--- src/components/MostRead.vue
+++ src/components/MostRead.vue
@@ -1,7 +1,7 @@
 <template>
-  <p class="text-lg font-bold mb-2">Most Read</p>
-  <div class="flex gap-3">
-    <div class="flex-1 flex flex-col gap-2 justify-around">
+  <p class="lg:text-lg text-2xl font-bold mb-0">Most Read</p>
+  <div class="flex gap-3 py-2 lg:flex-col 2xl:flex-row lg:border-b-0 border-b-2">
+    <div class="flex-1 flex flex-col gap-2 justify-around 2xl:order-first">
       <div class="flex gap-4 items-center">
         <p class="px-2 py-1 border-2 text-sm font-semibold">Politics</p>
         <p class="text-[#898989] font-extralight text-sm font-semibold">April 9, 2024</p>
@@ -9,8 +9,21 @@
       <p class="text-base font-semibold">Nicola Sturgeon Reported to Police OverApparent Face Mask Law Breach</p>
       <button class="underline underline-offset-auto text-sm text-left font-medium">Continue Reading</button>
     </div>
-    <div class="flex-1 aspect-w-1 aspect-h-1">
-      <img src="https://ychef.files.bbci.co.uk/1600x900/p0g6v92f.webp" alt="avatar" class="object-cover h-44"/>
+    <div class="flex-1 aspect-w-1 aspect-h-1 lg:order-first">
+      <img src="https://ychef.files.bbci.co.uk/1600x900/p0g6v92f.webp" alt="avatar" class="2xl:object-cover max-w-xl w-full  sm:h-44"/>
+    </div>
+  </div>
+  <div class="flex gap-3 py-2 lg:flex-col 2xl:flex-row lg:border-b-0 border-b-2">
+    <div class="flex-1 flex flex-col gap-2 justify-around 2xl:order-first">
+      <div class="flex gap-4 items-center">
+        <p class="px-2 py-1 border-2 text-sm font-semibold">Politics</p>
+        <p class="text-[#898989] font-extralight text-sm font-semibold">April 9, 2024</p>
+      </div>
+      <p class="text-base font-semibold">Nicola Sturgeon Reported to Police OverApparent Face Mask Law Breach</p>
+      <button class="underline underline-offset-auto text-sm text-left font-medium">Continue Reading</button>
+    </div>
+    <div class="flex-1 aspect-w-1 aspect-h-1 lg:order-first">
+      <img src="https://ychef.files.bbci.co.uk/1600x900/p0g6v92f.webp" alt="avatar" class="2xl:object-cover max-w-xl w-full  sm:h-44"/>
     </div>
   </div>
 </template>
