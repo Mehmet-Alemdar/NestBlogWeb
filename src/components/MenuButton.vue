c757dcc7c7da39d2e43b4c3957b00273c3d72941
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2024-01-10 14:35:46 +0300

    Make navbar responsive

---
+++ src/components/MenuButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex justify-end ml-2 px-4 py-1.5">
+    <button class="text-xl">
+      <font-awesome-icon icon="fa-solid fa-bars" />
+    </button>
+  </div>
+</template>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="w-11/12 flex justify-center border-b-2 p-6 m-4">
+  <div class="w-11/12 flex justify-center border-b-2 p-4 m-4">
     <div class="bg-white flex-1 max-w-screen-2xl text-center relative">
       <h1 class="font-black text-2xl absolute inset-x-0 mx-auto w-36 mt-2">NESTBLOG</h1>
-      <LoginButtons />
+      <div class="flex justify-end">
+        <div class="ml-auto hidden md:flex">
+          <LoginButtons />
+        </div>
+        <div class="ml-auto flex md:hidden">
+          <MenuButton />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
 import LoginButtons from './LoginButtons.vue';
+import MenuButton from './MenuButton.vue';
 </script>
