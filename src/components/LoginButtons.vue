90ff168d5dbab2c7b67e534352408c3f5b319ee7
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2024-01-10 16:58:07 +0300

    Add icon to sign in button

--- src/components/LoginButtons.vue
+++ src/components/LoginButtons.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="flex justify-end">
-    <button class="ml-2 bg-[#C7EC0E] px-4 py-1.5 rounded">Sign in</button>
-    <button class="ml-2 px-4 py-1.5 rounded border border-[#C7EC0E]">Sign up</button>
+    <div class="flex items-center justify-center">
+      <div class="bg-[#171717] ml-2 text-[#fff] px-4 py-1.5 rounded flex flex-row items-center">
+        <font-awesome-icon icon="fa-solid fa-user" class="text-white mr-2" />
+        <button class="text-[#fff] ml-auto">Sign in</button>
+      </div>
+    </div>
+    <button class="ml-2 px-4 py-1.5 rounded border border-[#171717]">Sign up</button>
   </div>
 </template>
