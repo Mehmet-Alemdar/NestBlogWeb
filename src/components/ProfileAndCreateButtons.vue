afe755b3a8e864e25146a46ca31019e94e254751
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2024-01-14 15:08:03 +0300

    Create profile and create buttons

---
+++ src/components/ProfileAndCreateButtons.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex justify-end">
+    <router-link to="create">
+      <font-awesome-icon icon="fa-solid fa-plus" class="text-[#171717] ml-2 px-4 py-2.5 rounded border border-[#171717] hover:bg-[#171717] hover:text-white" />
+    </router-link>
+    <router-link to="profile">
+      <font-awesome-icon icon="fa-solid fa-user" class="text-[#171717] ml-2 px-4 py-2.5 rounded border border-[#171717] hover:bg-[#171717] hover:text-white" />
+    </router-link>
+  </div>
+</template>
