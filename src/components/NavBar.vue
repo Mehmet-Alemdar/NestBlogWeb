650ce7caff93b3fb03e29368ddb67973f4014f7c
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2024-01-14 15:07:46 +0300

    Update navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="w-11/12 flex justify-center border-b-2 p-4 my-4">
-    <div class="bg-white flex-1 max-w-screen-2xl text-center relative">
+  <div class="w-11/12 flex justify-center border-b-2 py-4 my-4">
+    <div class="bg-white flex-1 text-center relative">
       <router-link to="/" class="font-black text-2xl absolute inset-x-0 mx-auto w-36 mt-1">NESTBLOG</router-link>
       <div class="flex justify-end">
         <div class="ml-auto hidden lg:flex">
